8bbbc358faa30cdd96e5e59b3892458f6a78ce02
wlenig <30681316+wlenig@users.noreply.github.com>
2025-01-09 16:50:36 -0500

    Moved blog directory

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/data/blog"}),
+    loader: glob({ pattern: "**/*.md", base: "./data/blog"}),
     schema: z.object({
         title: z.string(),
         publishDate: z.date()
